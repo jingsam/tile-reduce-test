541ee468aaeddedde1c77da0bf3cba9b6a3806d9
jingsam <jingsam@163.com>
2015-11-30 19:42:28 +0800

    prettify js

--- count.js
+++ count.js
@@ -3,44 +3,44 @@
 var turf = require('turf');
 
 module.exports = function(tilelayers, tile, writeData, done) {
-    var length = 0;
-    var layer =  tilelayers.osm.osm;
+  var length = 0;
+  var layer = tilelayers.osm.osm;
 
-    var roadTypes = [
-        'motorway',
-        'trunk',
-        'primary',
-        'secondary',
-        'tertiary',
-        'unclassified',
-        'residential',
-        'road',
-    ];
+  var roadTypes = [
+    'motorway',
+    'trunk',
+    'primary',
+    'secondary',
+    'tertiary',
+    'unclassified',
+    'residential',
+    'road',
+  ];
 
-    for (var i = 0; i < layer.features.length; i++) {
-        var feature = layer.features[i];
-        if (roadTypes.indexOf(feature.properties.highway) > -1) {
-            length += line_length(feature, 'kilometers');
-        }
+  for (var i = 0; i < layer.features.length; i++) {
+    var feature = layer.features[i];
+    if (roadTypes.indexOf(feature.properties.highway) > -1) {
+      length += line_length(feature, 'kilometers');
     }
+  }
 
-    done(null, length);
+  done(null, length);
 };
 
 
 function line_length(feature, units) {
-    var length = 0;
+  var length = 0;
 
-    if (feature.geometry.type == 'LineString') {
-        length = turf.lineDistance(feature, units);
+  if (feature.geometry.type == 'LineString') {
+    length = turf.lineDistance(feature, units);
+  }
+
+  if (feature.geometry.type == 'MultiLineString') {
+    for (var i = 0; i < feature.geometry.coordinates.length; i++) {
+      var linestring = turf.linestring(feature.geometry.coordinates[i]);
+      length += turf.lineDistance(linestring, units);
     }
+  }
 
-    if (feature.geometry.type == 'MultiLineString') {
-        for (var i = 0; i < feature.geometry.coordinates.length; i++) {
-            var linestring = turf.linestring(feature.geometry.coordinates[i]);
-            length += turf.lineDistance(linestring, units);
-        }
-    }
-
-    return length;
+  return length;
 }
--- index.js
+++ index.js
@@ -6,37 +6,38 @@ var path = require('path');
 var numFeatures = 0;
 
 var remoteSources = [
-    // {
-        // name: 'osm',
-        // url: 'https://b.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6/{z}/{x}/{y}.vector.pbf?access_token=pk.eyJ1IjoibWFwZXIiLCJhIjoiY2loMzVxMzU2MHk0NndjbTV6ZDllczd1biJ9.OroifZ0opDCfHtxq7CzOlg',
-        // layers: ['road'],
-        // maxrate: 10,
-        // raw: true
-    // },
-    // {
-        // name: 'osm',
-        // url: 'http://vector.mapzen.com/osm/roads/{z}/{x}/{y}.mvt?api_key=vector-tiles-1b0ODIU',
-        // maxrate: 15,
-        // raw: true
-    // }，
-    {
-        name: 'osm',
-        mbtiles: path.join(__dirname, '/latest.planet.z12.mbtiles'),
-        layers: ['osm']
-    }
+  // {
+  // name: 'osm',
+  // url: 'https://b.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6/{z}/{x}/{y}.vector.pbf?access_token=pk.eyJ1IjoibWFwZXIiLCJhIjoiY2loMzVxMzU2MHk0NndjbTV6ZDllczd1biJ9.OroifZ0opDCfHtxq7CzOlg',
+  // layers: ['road'],
+  // maxrate: 10,
+  // raw: true
+  // },
+  // {
+  // name: 'osm',
+  // url: 'http://vector.mapzen.com/osm/roads/{z}/{x}/{y}.mvt?api_key=vector-tiles-1b0ODIU',
+  // maxrate: 15,
+  // raw: true
+  // }，
+  {
+    name: 'osm',
+    mbtiles: path.join(__dirname, '/latest.planet.z12.mbtiles'),
+    layers: ['osm']
+  }
 ];
 
 tileReduce({
     // bbox: [-122.05862045288086, 36.93768132842635, -121.97296142578124, 37.00378647456494],
-    bbox: [-180.0,-85.1,180.0,85.1],
+    // bbox: [-180.0,-85.1,180.0,85.1],
+    bbox: [73.66, 3.86, 135.05, 53.55],
     // tiles: [[5276, 12757, 15]],
     zoom: 12,
     map: path.join(__dirname, '/count.js'),
     sources: remoteSources
-})
-.on('reduce', function(num) {
+  })
+  .on('reduce', function(num) {
     numFeatures += num;
-})
-.on('end', function() {
+  })
+  .on('end', function() {
     console.log('total: %d', numFeatures);
-});
+  });
