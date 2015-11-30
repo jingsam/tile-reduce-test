cedf9926cf7fb9d232ac2609b68d01837856ce9a
jingsam <jingsam@163.com>
2015-11-30 18:02:22 +0800

    total length of roads

--- count.js
+++ count.js
@@ -1,8 +1,46 @@
 'use strict';
 
-module.exports = function(data, tile, writeData, done) {
-  var count = 0;
-  // console.log(data);
-  if (data.mapzen_osm.roads) count += data.mapzen_osm.roads.length;
-  done(null, count);
+var turf = require('turf');
+
+module.exports = function(tilelayers, tile, writeData, done) {
+    var length = 0;
+    var layer =  tilelayers.osm.osm;
+
+    var roadTypes = [
+        'motorway',
+        'trunk',
+        'primary',
+        'secondary',
+        'tertiary',
+        'unclassified',
+        'residential',
+        'road',
+    ];
+
+    for (var i = 0; i < layer.features.length; i++) {
+        var feature = layer.features[i];
+        if (roadTypes.indexOf(feature.properties.highway) > -1) {
+            length += line_length(feature, 'kilometers');
+        }
+    }
+
+    done(null, length);
 };
+
+
+function line_length(feature, units) {
+    var length = 0;
+
+    if (feature.geometry.type == 'LineString') {
+        length = turf.lineDistance(feature, units);
+    }
+
+    if (feature.geometry.type == 'MultiLineString') {
+        for (var i = 0; i < feature.geometry.coordinates.length; i++) {
+            var linestring = turf.linestring(feature.geometry.coordinates[i]);
+            length += turf.lineDistance(linestring, units);
+        }
+    }
+
+    return length;
+}
--- index.js
+++ index.js
@@ -6,32 +6,37 @@ var path = require('path');
 var numFeatures = 0;
 
 var remoteSources = [
-  // {
-  //   name: 'streets',
-  //   url: 'https://b.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6/{z}/{x}/{y}.vector.pbf?access_token=pk.eyJ1IjoibWFwZXIiLCJhIjoiY2loMzVxMzU2MHk0NndjbTV6ZDllczd1biJ9.OroifZ0opDCfHtxq7CzOlg',
-  //   layers: ['road'],
-  //   maxrate: 10,
-  //   raw: true
-  // },
-  {
-    name: 'mapzen_osm',
-    url: 'http://vector.mapzen.com/osm/roads/{z}/{x}/{y}.mvt?api_key=vector-tiles-1b0ODIU',
-    maxrate: 10,
-    raw: true
-  }
+    // {
+        // name: 'osm',
+        // url: 'https://b.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6/{z}/{x}/{y}.vector.pbf?access_token=pk.eyJ1IjoibWFwZXIiLCJhIjoiY2loMzVxMzU2MHk0NndjbTV6ZDllczd1biJ9.OroifZ0opDCfHtxq7CzOlg',
+        // layers: ['road'],
+        // maxrate: 10,
+        // raw: true
+    // },
+    // {
+        // name: 'osm',
+        // url: 'http://vector.mapzen.com/osm/roads/{z}/{x}/{y}.mvt?api_key=vector-tiles-1b0ODIU',
+        // maxrate: 15,
+        // raw: true
+    // }，
+    {
+      name: 'osm',
+      mbtiles: path.join(__dirname, '/latest.planet.z12.mbtiles'),
+      layers: ['osm']
+    }
 ];
 
 tileReduce({
-  bbox: [-122.05862045288086, 36.93768132842635, -121.97296142578124, 37.00378647456494],
-  // bbox: [-180.0,-85.1,180.0,85.1],
-  // tiles: [[5276, 12757, 15]],
-  zoom: 15,
-  map: path.join(__dirname, '/count.js'),
-  sources: remoteSources
+    // bbox: [-122.05862045288086, 36.93768132842635, -121.97296142578124, 37.00378647456494],
+    bbox: [-180.0,-85.1,180.0,85.1],
+    // tiles: [[5276, 12757, 15]],
+    zoom: 12,
+    map: path.join(__dirname, '/count.js'),
+    sources: remoteSources
 })
 .on('reduce', function(num) {
-  numFeatures += num;
+    numFeatures += num;
 })
 .on('end', function() {
-  console.log('Features total: %d', numFeatures);
+    console.log('total: %d', numFeatures);
 });
